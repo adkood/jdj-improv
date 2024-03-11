3d2ba293be4a3583a19413268abc33e4c99fd0d0
adkood <ashutoshnegi195@gmail.com>
2024-03-11 15:27:06 +0530

    animation threshold changed

--- jdj/src/utils/GetAnimation.js
+++ jdj/src/utils/GetAnimation.js
@@ -4,7 +4,7 @@ import { useInView } from 'react-intersection-observer';
 const GetAnimation = ({ children, beforeView = '', afterView = '' }) => {
     const [ref, inView] = useInView({
         triggerOnce: true,
-        threshold: 0.8,
+        threshold: 0.5,
     });
 
     return (
