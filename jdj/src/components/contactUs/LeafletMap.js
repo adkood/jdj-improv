7b3eea88ef2803bae1a43f577bdade511b7f523a
adkood <ashutoshnegi195@gmail.com>
2024-03-16 22:11:33 +0530

    still trying

--- jdj/src/components/contactUs/LeafletMap.js
+++ jdj/src/components/contactUs/LeafletMap.js
@@ -2,18 +2,20 @@
 
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css"; 
+import "leaflet/dist/leaflet.css";
 
 const LeafletMap = ({ center, zoom }) => {
   return (
-    <MapContainer center={center} zoom={zoom} style={{ height: "100%", width: "100%" , zIndex: "1"}}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={center}>
-        <Popup>
-          JDJ Pvt. Lmt.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <>
+      {(typeof window !== "undefined") ? <MapContainer center={center} zoom={zoom} style={{ height: "100%", width: "100%", zIndex: "1" }}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={center}>
+          <Popup>
+            JDJ Pvt. Lmt.
+          </Popup>
+        </Marker>
+      </MapContainer> : <div>loading...</div>}
+    </>
   );
 };
 
