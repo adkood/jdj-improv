3a5fffc9f49114196984c8c40ffd8fb1af023cf4
adkood <ashutoshnegi195@gmail.com>
2024-03-17 16:03:07 +0530

    reverted changes

--- jdj/src/components/contactUs/LeafletMap.js
+++ jdj/src/components/contactUs/LeafletMap.js
@@ -2,31 +2,17 @@
 
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from "leaflet"; // Import Leaflet library
 import "leaflet/dist/leaflet.css";
 import { Spinner } from "@chakra-ui/react";
-
-
-let redIcon;
-
-if (typeof window !== 'undefined') {
-  redIcon = new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-  })
-}
 
 const LeafletMap = ({ center, zoom }) => {
   return (
     <>
       {(typeof window !== "undefined") ? <MapContainer center={center} zoom={zoom} style={{ height: "100%", width: "100%", zIndex: "1" }}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <Marker position={center} icon={redIcon}>
+        <Marker position={center}>
           <Popup>
-            Over Here!
+            Over Here !
           </Popup>
         </Marker>
       </MapContainer> : <Spinner
